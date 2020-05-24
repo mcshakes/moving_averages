37b981edb05e897702707e004c529bec14c043b6
mcshakes <efmccarthy26@gmail.com>
2020-05-24 17:04:10 -0600

    May need to just make these helper functions

--- lib/simple_moving_average.py
+++ lib/simple_moving_average.py
@@ -7,45 +7,53 @@ import matplotlib.pyplot as plt
 
 class SimpleMovingAverage:
 
+    def __init__(self):
+        data = None
+        trade_buy = []
+        trade_sell = []
+
     def import_data(self, ticker, date):
-        data_acb = pandas_data_reader.get_data_yahoo(ticker, date)
-        print(data_acb)
-# SMA = Simple Moving Average
+        data_returned = pandas_data_reader.get_data_yahoo(ticker, date)
+        data_returned['2_SMA'] = data_returned['Close'].rolling(
+            window=2).mean()
 
+        data_returned['5_SMA'] = data_returned['Close'].rolling(
+            window=5).mean()
 
-# data_acb['2_SMA'] = data_acb['Close'].rolling(window=2).mean()
-# data_acb['5_SMA'] = data_acb['Close'].rolling(window=5).mean()
+        data_returned = data_returned[data_returned['5_SMA'].notna()]
 
-# data_acb = data_acb[data_acb['5_SMA'].notna()]
+        self.data = data_returned
 
-# Trade_Buy = []
-# Trade_Sell = []
+    def moving_crossover(self, data):
+        # Trade_Buy = []
+        # Trade_Sell = []
+        for i in range(len(self.data)-1):
+            if ((data['2_SMA'].values[i] < data['5_SMA'].values[i]) & (data['2_SMA'].values[i+1] > data['5_SMA'].values[i+1])):
+                print("Trade Call for {row} is Buy.".format(
+                    row=data.index[i].date()))
+                trade_buy.append(i)
+            elif ((data['2_SMA'].values[i] > data['5_SMA'].values[i]) & (data['2_SMA'].values[i+1] < data['5_SMA'].values[i+1])):
+                print("Trade Call for {row} is Sell.".format(
+                    row=data.index[i].date()))
+                trade_buy.append(i)
 
-# for i in range(len(data_acb)-1):
-#     if ((data_acb['2_SMA'].values[i] < data_acb['5_SMA'].values[i]) & (data_acb['2_SMA'].values[i+1] > data_acb['5_SMA'].values[i+1])):
-#         print("Trade Call for {row} is Buy.".format(
-#             row=data_acb.index[i].date()))
-#         Trade_Buy.append(i)
-#     elif ((data_acb['2_SMA'].values[i] > data_acb['5_SMA'].values[i]) & (data_acb['2_SMA'].values[i+1] < data_acb['5_SMA'].values[i+1])):
-#         print("Trade Call for {row} is Sell.".format(
-#             row=data_acb.index[i].date()))
-#         Trade_Sell.append(i)
+        print(self.trade_buy)
+        print(self.trade_sell)
 
-# plt.figure(figsize=(20, 10), dpi=80)
-# plt.plot(data_acb.index, data_acb['Close'])
-# plt.plot(data_acb.index, data_acb['2_SMA'], '-^',
-#          markevery=Trade_Buy, ms=15, color='green')
-# plt.plot(data_acb.index, data_acb['5_SMA'], '-v',
-#          markevery=Trade_Sell, ms=15, color='red')
-# plt.xlabel('Date', fontsize=14)
-# plt.ylabel('Price in Dollars', fontsize=14)
-# plt.xticks(rotation='60', fontsize=12)
-# plt.yticks(fontsize=12)
-# plt.title('Trade Calls - Moving Averages Crossover', fontsize=16)
-# plt.legend(['Close', '2_SMA', '5_SMA'])
-# plt.grid()
-# plt.show()
+        # plt.figure(figsize=(20, 10), dpi=80)
+        # plt.plot(data_returned.index, data_returned['Close'])
+        # plt.plot(data_returned.index, data_returned['2_SMA'], '-^',
+        #          markevery=Trade_Buy, ms=15, color='green')
+        # plt.plot(data_returned.index, data_returned['5_SMA'], '-v',
+        #          markevery=Trade_Sell, ms=15, color='red')
+        # plt.xlabel('Date', fontsize=14)
+        # plt.ylabel('Price in Dollars', fontsize=14)
+        # plt.xticks(rotation='60', fontsize=12)
+        # plt.yticks(fontsize=12)
+        # plt.title('Trade Calls - Moving Averages Crossover', fontsize=16)
+        # plt.legend(['Close', '2_SMA', '5_SMA'])
+        # plt.grid()
+        # plt.show()
 
-
-# https://blog.quantinsti.com/moving-average-trading-strategies/
-# Read Simple Moving Averages
+        # https://blog.quantinsti.com/moving-average-trading-strategies/
+        # Read Simple Moving Averages
